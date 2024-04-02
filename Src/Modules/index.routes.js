08ecabcb823210f9d8b3d74bcdc90e05d0cc0521
AbullahUsama087 <abdullah.usama087@gmail.com>
2024-04-02 21:44:19 +0200

    Modules Controllers and Routes

---
+++ Src/Modules/index.routes.js
@@ -0,0 +1,7 @@
+import userRouter from "./User/user.routes.js";
+
+import schoolRouter from "./School/school.routes.js";
+
+import classroomRouter from "./Classroom/classroom.routes.js";
+
+export { userRouter, schoolRouter, classroomRouter };
