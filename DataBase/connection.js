e3cfe7602f6193f41b34fa6e61f892b4c75ce2c2
AbullahUsama087 <abdullah.usama087@gmail.com>
2024-04-02 21:30:24 +0200

    The Connection file to DataBase

---
+++ DataBase/connection.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const DbConnection = async () => {
+  return await mongoose
+    .connect(process.env.DB_CONNECTION_URL)
+    .then((res) => console.log("Database connection established successfully"))
+    .catch((err) => console.error("Error connecting to Database", err));
+};
+
+export default DbConnection;
