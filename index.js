697a4de20056537574d064547e6505285b45565b
AbullahUsama087 <abdullah.usama087@gmail.com>
2024-04-02 21:32:49 +0200

    Index file of the project

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+
+import { config } from "dotenv";
+
+import path from "path";
+
+config({ path: path.resolve("./Config/config.env") });
+
+import DbConnection from "./DataBase/connection.js";
+import * as Routers from "./Src/Modules/index.routes.js";
+
+import {
+  globalErrorResponse,
+  globalNotFoundPageError,
+} from "./Src/Utils/errorhandling.js";
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+DbConnection();
+
+app.use("/user", Routers.userRouter);
+app.use("/school", Routers.schoolRouter);
+app.use("/classroom", Routers.classroomRouter);
+
+app.all("*", globalNotFoundPageError);
+
+app.use(globalErrorResponse);
+
+app.listen(port, () => console.log("Server listening on port successfully"));
