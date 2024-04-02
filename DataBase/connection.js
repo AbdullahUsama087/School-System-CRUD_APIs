ca449b08d5dcf74c2e356dd5224066054eeb2f14
AbullahUsama087 <abdullah.usama087@gmail.com>
2024-04-02 22:34:36 +0200

    Upload on Render Host

--- DataBase/connection.js
+++ DataBase/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const DbConnection = async () => {
   return await mongoose
-    .connect(process.env.DB_CONNECTION_URL)
+    .connect(process.env.DB_CONNECTION_CLOUD)
     .then((res) => console.log("Database connection established successfully"))
     .catch((err) => console.error("Error connecting to Database", err));
 };
